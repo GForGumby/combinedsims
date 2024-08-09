cba29d0d151cf55ca6baa9f28e75d82419c15ab4
Gumby <156488743+GForGumby@users.noreply.github.com>
2024-08-09 11:21:40 -0400

    Update app.py

--- app.py
+++ app.py
@@ -9,4 +9,4 @@ st.sidebar.success("Select a page above.")
 
 st.text("Instructions: Navigate to Draft tab and download the adp sheet. In there you will find the adp template. Download this and make changes if needed (specifically to adpsd). Upload into the simulator.")
 st.text("Wait for the simulation to finish, (takes approx 2 mins per 1000 drafts). Make sure you download the results")
-st.text("Navigate to the Projections page, upload your draft results. Upload your projections. Must be in the format of player_name, projection in column A and B of your csv")
+st.text("Navigate to the Projections page, upload your draft results. Upload your projections. Must be in the format of player_name, proj in column A and B of your csv")
