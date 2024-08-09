3e6367c6aab88428929e9c80f81f2ef70892652d
Gumby <156488743+GForGumby@users.noreply.github.com>
2024-08-09 10:58:41 -0400

    Update and rename app.py to Instructions.py

---
+++ Instructions.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Gumby Sims",
+)
+
+st.title("Main Page")
+st.sidebar.success("Select a page above.")
+st.text(Instructions: 
+Use the draft page on the sidebar to simulate the draft. I have a sample adp posted)
