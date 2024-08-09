9e850c2e289cca73b519505aa1376f6e34a16abe
Gumby <156488743+GForGumby@users.noreply.github.com>
2024-08-09 13:04:22 -0400

    Update Projections.py

--- pages/Projections.py
+++ pages/Projections.py
@@ -260,7 +260,7 @@ def simulate_team_projections(draft_results, player_positions, player_teams, pro
     return avg_payouts
 
 # Function to run parallel simulations
-def run_parallel_simulations(num_simulations, draft_results_df, projection_lookup):
+run_parallel_simulations(num_simulations, draft_results_df, projection_lookup):
     draft_results, player_positions, player_teams, teams = prepare_draft_results(draft_results_df)
     avg_payouts = simulate_team_projections(draft_results, player_positions, player_teams, projection_lookup, num_simulations)
 
