c0f361715eac7965b56f3cc53235682d591b4dff
Gumby <156488743+GForGumby@users.noreply.github.com>
2024-08-23 11:38:13 -0400

    Update draftandproj1code.py

--- pages/draftandproj1code.py
+++ pages/draftandproj1code.py
@@ -2,6 +2,22 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+st.markdown('### Already have an Account? Login Below👇🏻')
+with st.form("login_form"):
+    st.write("Login")
+    email = st.text_input('Enter Your Email')
+    password = st.text_input('Enter Your Password')
+    submitted = st.form_submit_button("Login")
+
+
+if submitted:
+    if password == config('SECRET_PASSWORD'):
+        st.session_state['logged_in'] = True
+        st.text('Succesfully Logged In!')
+    else:
+        st.text('Incorrect, login credentials.')
+        st.session_state['logged_in'] = False
+
 if 'logged_in' in st.session_state.keys():
     if st.session_state['logged_in']:
         st.markdown('## Ask Me Anything')
