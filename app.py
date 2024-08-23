45516ff514169369a9ed018e9fee03c06bfc7f73
Gumby <156488743+GForGumby@users.noreply.github.com>
2024-08-23 12:27:42 -0400

    Update app.py

--- app.py
+++ app.py
@@ -14,5 +14,5 @@ st.markdown("Instructions: Navigate to Draft tab and download the adp sheet. In 
 st.markdown("Wait for the simulation to finish, (takes approx 2 mins per 1000 drafts). Make sure you download the results")
 st.write("Navigate to the Projections page, upload your draft results. Upload your projections. Must be in the format of player_name, proj in column A and B of your csv")
 
-st.write("google-site-verification" content="45GIE4pR1aI7CMZtcceMzubFU42Y3jwBP5gFyAIaiuc")
+st.write("google-site-verification") (content="45GIE4pR1aI7CMZtcceMzubFU42Y3jwBP5gFyAIaiuc")
 
